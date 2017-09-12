3e55bfad13933b2e1b84ee271f31893586a7b0d8
thebiggeek <24227623+thebiggeek@users.noreply.github.com>
2017-09-12 20:33:18 +0530

    Update mailq-higher-than-trigger.sh

--- mailq-higher-than-trigger.sh
+++ mailq-higher-than-trigger.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 MAILQ="$(mailq | tail -1  | awk '{print $5}')"
 echo "${MAILQ}"
 MDMAILS="$(mailq | grep MAILER-DAEMON | wc -l)"
@@ -7,7 +8,11 @@ if [ $MDMAILS \> $THIRDMAILQ ];
 then
 	echo "Mailer Daemon Mailes Nearly Third of Mail Queue";
 	echo "Will now Process Command to delete Mailer Daemon Emails";
- /root/mailer-daemon-delete.sh
+mailq | tail -n +2 | grep -v '^ *(' | awk  'BEGIN { RS = "" }
+# $7=sender, $8=recipient1, $9=recipient2
+{ if ($7 == "MAILER-DAEMON" )
+    print $1 }
+' | tr -d '*!' | postsuper -d -
 MAILQ="$(mailq | tail -1  | awk '{print $5}')"
 MDMAILS="$(mailq | grep MAILER-DAEMON | wc -l)"
 	echo "Now Mail Queue is" $MAILQ "and Mailer Daemon Mails are" $MDMAILS "";
@@ -15,3 +20,4 @@ else
 	echo "Mailer Daemon Mails only" $MDMAILS "While Mail Queue is" $MAILQ "";
 fi;
 OLDESTEMAIL="postqueue -p | head -2 | tail -1 | awk '{ print $4, $5 }'"
+echo "Currently the Oldest Email in Queue is from" $OLDESTEMAIL;
